0f9a640e25945c204ade184389246e07fc4055f1
Tanbir Hossain <mdtanbirhossain1433@gmail.com>
2023-11-25 23:14:29 +0600

    heeloa

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Test extends StatefulWidget {
   final String name;
@@ -16,21 +16,21 @@ class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          widget.name,
-          style: isChecked
-              ? const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 17.0,
-                  decoration: TextDecoration.lineThrough)
-              : const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 17.0,
-                ),
-        ),
-      trailing:Checkbox(
+      title: Text(
+        widget.name,
+        style: isChecked
+            ? const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontFamily: 'SourceSansPro',
+                fontSize: 17.0,
+                decoration: TextDecoration.lineThrough)
+            : const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontFamily: 'SourceSansPro',
+                fontSize: 17.0,
+              ),
+      ),
+      trailing: Checkbox(
         value: isChecked,
         onChanged: (bool? value) {
           setState(() {
@@ -39,45 +39,41 @@ class _TestState extends State<Test> {
         },
       ),
 
+      //*************************************
 
-
-        
-        //*************************************
-
-        // trailing: GestureDetector(
-        //   onTap: () {
-        //     setState(() {
-        //       isChecked = !isChecked;
-        //     });
-        //   },
-        //   child: Container(
-        //     height: 25,
-        //     width: 25,
-        //     decoration: BoxDecoration(
-        //         border: Border.all(
-        //       color: Colors.black,
-        //       width: 2,
-        //     )),
-        //     child: isChecked
-        //         ? const Center(
-        //             child: FaIcon(
-        //               FontAwesomeIcons.check,
-        //               color: Colors.lightBlueAccent,
-        //               size: 18,
-        //             ),
-        //           )
-        //         : const Center(
-        //             child: FaIcon(
-        //               FontAwesomeIcons.galacticRepublic,
-        //               color: Colors.white,
-        //               size: 1,
-        //             ),
-        //           ),
-        //   ),
-        // )
-        //onTap: () {},
+      // trailing: GestureDetector(
+      //   onTap: () {
+      //     setState(() {
+      //       isChecked = !isChecked;
+      //     });
+      //   },
+      //   child: Container(
+      //     height: 25,
+      //     width: 25,
+      //     decoration: BoxDecoration(
+      //         border: Border.all(
+      //       color: Colors.black,
+      //       width: 2,
+      //     )),
+      //     child: isChecked
+      //         ? const Center(
+      //             child: FaIcon(
+      //               FontAwesomeIcons.check,
+      //               color: Colors.lightBlueAccent,
+      //               size: 18,
+      //             ),
+      //           )
+      //         : const Center(
+      //             child: FaIcon(
+      //               FontAwesomeIcons.galacticRepublic,
+      //               color: Colors.white,
+      //               size: 1,
+      //             ),
+      //           ),
+      //   ),
+      // )
+      //onTap: () {},
       //******************************************
-
-        );
+    );
   }
 }
